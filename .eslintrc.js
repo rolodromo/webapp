ef5592810f233da6ddf3a11b70093d70997add64
guumaster <guuweb@gmail.com>
2018-06-12 22:21:04 +0200

    add vuetify

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,26 +3,12 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/prettier'
-  ],
+  extends: ['plugin:vue/essential', '@vue/prettier'],
   rules: {
-    'no-console': 'off',
-    'no-debugger': 'off',
-    'vue/attribute-hyphenation': [
-      'error',
-      'always'
-    ],
-    'vue/html-indent': [
-      'error',
-      2
-    ],
+    'vue/attribute-hyphenation': ['error', 'always'],
+    'vue/html-indent': ['error', 2],
     'vue/require-prop-types': 'error',
-    'vue/html-quotes': [
-      'error',
-      'single'
-    ],
+    'vue/html-quotes': ['error', 'single'],
     'vue/order-in-components': 'error'
   },
   parserOptions: {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,43 @@
 <template>
   <v-container fluid>
-    <v-slide-y-transition mode="out-in">
+    <v-slide-y-transition mode='out-in'>
       <v-layout column align-center>
-        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
-        <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;John Johnson</em>
-            </small>
-          </footer>
-        </blockquote>
+        <img src='../assets/icon-512x512.png' class='mb-5'>
+        <HelloWorld msg='Bienvenido a Rolodromo.com'/>
       </v-layout>
     </v-slide-y-transition>
   </v-container>
 </template>
 
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
